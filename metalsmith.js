3dccdcb83ff82ef58736c354ee7fca08d99f8d61
kunesv <kunesv@sprinters.cz>
2016-05-02 23:18:56 +0200

    Cleaning Mapbox, Style updates & some markdown tests

--- metalsmith.js
+++ metalsmith.js
@@ -4,40 +4,46 @@ var markdown = require('metalsmith-markdown');
 var permalinks = require('metalsmith-permalinks');
 var dateFormatter = require('metalsmith-date-formatter');
 var layouts = require('metalsmith-layouts');
+var sass = require('metalsmith-sass');
 var serve = require('metalsmith-serve');
 var watch = require('metalsmith-watch');
 
 Metalsmith(__dirname)
     .use(collections({
-        "schuze": {
-            "pattern": "zapisy/schuze/**/*.*",
-            "sortBy": "date"
+        schuze: {
+            pattern: 'zapisy/schuze/**/*.*',
+            sortBy: 'date'
         },
-        "spravni": {
-            "pattern": "zapisy/spravni_rada/**/*.*",
-            "sortBy": "date"
+        spravni: {
+            pattern: 'zapisy/spravni_rada/**/*.*',
+            sortBy: 'date'
         }
     }))
-    .use(markdown())
+    .use(markdown({
+        gfm: true
+    }))
     .use(permalinks(
         {
-            "pattern": ":path:date",
-            "relative": false
+            pattern: ':path:date',
+            relative: false
         }
     ))
     .use(dateFormatter({
-        "dates": [{
-            "key": "date",
-            "format": "D. M. YYYY"
+        dates: [{
+            key: 'date',
+            format: 'D. M. YYYY'
         }]
     }))
     .use(layouts({
-        "engine": "handlebars",
-        "directory": "src/layouts"
+        engine: 'handlebars',
+        directory: 'src/layouts'
+    }))
+    .use(sass({
+        outputDir: 'css/'
     }))
     .use(serve({
-        "host": "0.0.0.0",
-        "port": process.env.PORT || 5000
+        host: '0.0.0.0',
+        port: process.env.PORT || 5000
     }))
     .use(watch())
     .build(function (err) {
